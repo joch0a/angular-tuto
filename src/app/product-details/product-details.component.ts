0a0889d7d6b9a4285f082b52b316727a528fdf14
joch0a <ochoajoseignacio.96@gmail.com>
2021-01-29 18:03:55 -0300

    Next step http client requests

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
+import { CartService } from "../cart.service";
 import { products } from "../products";
 
 @Component({
@@ -10,7 +11,10 @@ import { products } from "../products";
 export class ProductDetailsComponent implements OnInit {
   product;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
 
   ngOnInit() {
     // First get the product id from the current route.
@@ -19,5 +23,10 @@ export class ProductDetailsComponent implements OnInit {
 
     // Find the product that correspond with the id provided in route.
     this.product = products.find(product => product.id === productIdFromRoute);
+  }
+
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert("Your product has been added to the cart!");
   }
 }
